a90e0638b1d59561656094f5cd56c1e58fa55091
islam mohamed <islam.mohamed.alrfa3i@gmail.com>
2025-09-08 00:29:15 +0300

    fix deploy

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,66 +1,10 @@
-"use client";
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter, useSearchParams } from "next/navigation";
-import Form from "@components/Form";
-export default function UpdatePrompt() {
-  const [submitting, setsubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
-  const router = useRouter();
-  const { data: session } = useSession();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
+import { Suspense } from "react";
+import UpdatePrompt from "./UpdatePrompt";
 
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    };
-
-    if (promptId) getPromptDetails();
-  }, [promptId]);
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setsubmitting(true);
-    if (!promptId) return alert("Prompt ID not found");
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
-      console.log(response);
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setsubmitting(false);
-    }
-  };
+export default function Page() {
   return (
-    <div>
-      <Form
-        type="Update"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
-    </div>
+    <Suspense fallback={<span className="loading loading-spinner"></span>}>
+      <UpdatePrompt />
+    </Suspense>
   );
 }
